5125b198c794c797554005c2da6a9cc72c5de007
lauraGS <lauraseg13@gmail.com>
2015-07-23 21:39:52 +0200

    Crear preguntas

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(partials());
 app.use(favicon(__dirname + '/public/favicon2.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -44,4 +44,22 @@ exports.answer = function(req, res) {
 };
 
 
+// GET /quizes/new
+exports.new = function(req, res) {
+  var quiz = models.Quiz.build( // crea objeto quiz 
+    {pregunta: "Pregunta", respuesta: "Respuesta"}
+  );
 
+  res.render('quizes/new', {quiz: quiz});
+};
+
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+	//guarda en BD los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+	res.redirect('/quizes');
+	})
+		
+	};
