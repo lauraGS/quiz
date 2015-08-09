9e4bad462c84d34c04abe2a5f8902117190b57f1
lauraGS <lauraseg13@gmail.com>
2015-08-09 21:13:24 +0200

    Ampliación opcional: página de estadisticas

---
+++ controllers/statistics_controller.js
@@ -0,0 +1,35 @@
+var models = require('../models/models.js');
+
+// GET /statics
+exports.load = function(req, res){
+	var datos = {};
+		
+	//número de preguntas
+	models.Quiz.findAndCountAll().then(function(result){
+		datos.numPreguntas = result.count;
+		
+		models.Comment.findAndCountAll().then(function(result){
+			//número de comentarios totales
+				datos.numComentarios = result.count;
+			//número medio de comentarios por pregunta
+			datos.numComentariosMedio = datos.numComentarios / datos.numPreguntas;
+				
+			
+			
+			models.Comment.findAll({group: 'QuizId'}).then(function(result){
+					//número de preguntas con comentarios
+					datos.numPregComent = result.length;
+					
+					//número preguntas sin comentarios
+					datos.numPregSinComent = datos.numPreguntas - datos.numPregComent;
+					
+
+				
+				res.render('statistics/statistics', {statistics: datos, errors:[]});
+			});
+		});	
+	}).catch(function(error){next(error)});
+
+	
+	
+};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
+var statisticsController = require('../controllers/statistics_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -39,5 +40,8 @@ router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 	sessionCo
 router.get('/author', function(req, res) {
  res.render('author', {title: 'Author', errors: []});
 });
+
+//para la página de estadísticas
+router.get('/quizes/statistics', statisticsController.load);
 
 module.exports = router;
