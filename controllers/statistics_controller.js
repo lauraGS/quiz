777b342f046eba337f597c1c72ca15dc56991a60
lauraGS <lauraseg13@gmail.com>
2015-08-11 17:44:31 +0200

    modificación estadísticas

--- controllers/statistics_controller.js
+++ controllers/statistics_controller.js
@@ -13,16 +13,25 @@ exports.load = function(req, res){
 				datos.numComentarios = result.count;
 			//número medio de comentarios por pregunta
 			datos.numComentariosMedio = datos.numComentarios / datos.numPreguntas;
-				
-			models.Comment.findAll({group: ['QuizId']}).then(function(result){
-					//número de preguntas con comentarios
-					datos.numPregComent = result.length;
-					
+			
+			models.Quiz.findAll({include: [{ model: models.Comment }]}).then(function (result) {
+				if (result) { 
+					var numPregNoComments = 0;
+					result.forEach(function (quiz) {
+						if (quiz.Comments.length === 0) {
+							numPregNoComments++;
+						}
+					});
 					//número preguntas sin comentarios
-					datos.numPregSinComent = datos.numPreguntas - datos.numPregComent;
-					
-
-				
+					datos.numPregSinComent = numPregNoComments;
+					//número de preguntas con comentarios
+					datos.numPregComent = datos.numPreguntas - datos.numPregSinComent;
+				}else{
+					datos.numPregSinComent = 0;
+					datos.numPregComent = 0;
+				}
+			
+			
 				res.render('statistics/statistics', {statistics: datos, errors:[]});
 			});
 		});	
